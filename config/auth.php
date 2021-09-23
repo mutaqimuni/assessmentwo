38d32d9beab854758767120479ee2abed03c853f
mutaqimuni <mutaqim.unijaya@gmail.com>
2021-09-23 11:04:19 +0800

    Add api auth configuration: change if not work

--- config/auth.php
+++ config/auth.php
@@ -77,7 +77,7 @@ return [
         // ],
     ],
 
-    // Add api auth configuration
+    // Add api auth configuration- test
     'guards' => [
         'web' => [
             'driver' => 'session',
